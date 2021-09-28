84e8dd7bf8dde363abea0b8c5b730f5bfcfd2528
Opilane TTHK <opilane@tthk.ee>
2021-09-28 13:59:01 +0300

    Week5-Code-ReView

---
+++ InstagramAgeValidation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace InstagramAgeValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajalt sünniaastat
+            //programm arvutab kasutaja vanust
+            //programm kontrollib, kas kasutaja on vanem kui 13 vüi noorem kui 13 aastat vana
+            //kui kasutaja on 13 aastat vana või vanem,
+            //siis ta võõib Instagrami kasutada
+            //kui kasutaja on noorem, kui 13, siis ta on liiga noor
+
+            Console.WriteLine("Sisestada sünniaasta");
+            int YearOfBirth = Convert.ToInt32(Console.ReadLine());
+            int Age = 2021 - YearOfBirth;
+
+            if (Age >= 13)
+            {
+                Console.WriteLine("Võid Instagrami kasutada.");
+
+            }
+            else
+            {
+                Console.WriteLine("Oled liiga noor.");
+            }
+        }
+    }
+}
+    
+
